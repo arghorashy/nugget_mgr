1be129449e0c945d641d1a7b4c3dc0be7adaec62
Afsheen Ghorashy <arghorashy@gmail.com>
2013-10-06 13:38:30 -0400

    Created nuggets controller.
    Signed-off-by: Afsheen Ghorashy <arghorashy@gmail.com>

---
+++ app/controllers/nuggets_controller.rb
@@ -0,0 +1,4 @@
+class NuggetsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 NuggetMgr::Application.routes.draw do
+  get "nuggets/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
