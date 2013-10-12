6dded927c1d19780bdab801362446c63adfea516
Afsheen Ghorashy <arghorashy@gmail.com>
2013-10-12 16:30:34 -0400

    Finished tutorial http://net.tutsplus.com/tutorials/ruby/the-intro-to-rails-screencast-i-wish-i-had/
    Signed-off-by: Afsheen Ghorashy <arghorashy@gmail.com>

--- app/controllers/nuggets_controller.rb
+++ app/controllers/nuggets_controller.rb
@@ -1,7 +1,45 @@
 class NuggetsController < ApplicationController
   def index
+  	@nugget = Nugget.new
   	@nuggets = Nugget.all
   end
 
+  def create
+  	#render :text => params[:nugget].inspect
+  	Nugget.create nugget_params
+  	redirect_to :back 
+
+  end
+
+  def edit
+  	@nugget = Nugget.find params[:id]
+  end
+
+  def update
+  	@nugget = Nugget.find params[:id]
+  	if @nugget.update_attributes nugget_params
+  		flash[:notice_good] = 'You nugget was successfully updated!'
+  		redirect_to nuggets_path
+  	else
+  		flash[:notice_bad] = 'There was an error updating your nugget!'
+  		redirect_to :back
+  	end
+  end
+
+  def destroy
+  	Nugget.destroy params[:id]
+  	flash[:notice_good] = "Nugget successfully deleted."
+  	redirect_to :back
+  end
+
+
+  private
+
+
+  # WHY???
+  def nugget_params
+    allow = [:name, :description, :markForReview]
+    params.require(:nugget).permit(allow)
+  end
 
 end
--- app/models/nugget.rb
+++ app/models/nugget.rb
@@ -1,2 +1,3 @@
 class Nugget < ActiveRecord::Base
+	validates :name, presence: true
 end
